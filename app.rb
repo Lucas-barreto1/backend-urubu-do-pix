1bebfa95e2922c3dd1ebd1756d87699ca5907c6b
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-06-26 21:43:45 -0300

    feat: add  router from orders

--- app.rb
+++ app.rb
@@ -5,11 +5,16 @@ require 'sinatra/activerecord'
 require 'dotenv'
 Dotenv.load
 
-require './app\adapters\controllers\users_controllers'
+require './app/adapters/controllers/users_controllers'
+require './app/adapters/controllers/orders_controllers'
 
 set :database, {adapter: 'sqlite3', database: ENV['DB_PATH']}
 
 
 post '/users' do
   {result: UsersController.create(params)}.to_json
+end
+
+post '/orders' do
+  {result: OrdersController.create(params)}.to_json
 end
