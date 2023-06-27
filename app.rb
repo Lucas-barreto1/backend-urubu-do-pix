d0169a3466d7b9180b3d158fa13cd40cf7bde6b8
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-06-26 22:04:30 -0300

    refactor: move user_id to route param

--- app.rb
+++ app.rb
@@ -11,10 +11,10 @@ require './app/adapters/controllers/orders_controllers'
 set :database, {adapter: 'sqlite3', database: ENV['DB_PATH']}
 
 
-post '/users' do
+post '/:user_id/users' do
   {result: UsersController.create(params)}.to_json
 end
 
 post '/orders' do
-  {result: OrdersController.create(params)}.to_json
+  {result: OrdersController.new(params[:user_id]).create(params)}.to_json
 end
--- app/model/transaction.rb
+++ app/model/transaction.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 class Transaction
-  attr_accessor :type, :value
   attr_accessor :transaction_type, :value, :order_id
 
-  def initialize(type:, value:)
-    @type = type
   def initialize(transaction_type:, value:, order_id:)
     @transaction_type = transaction_type
     @value = value
