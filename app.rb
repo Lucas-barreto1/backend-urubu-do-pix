918b499cd91666613d34e308562de5d3c53a88e6
Lucas Barreto <lucasviniciusbarreto25@gmail.com>
2023-06-26 22:06:55 -0300

    refactor: change param route

--- app.rb
+++ app.rb
@@ -11,10 +11,10 @@ require './app/adapters/controllers/orders_controllers'
 set :database, {adapter: 'sqlite3', database: ENV['DB_PATH']}
 
 
-post '/:user_id/users' do
+post '/users' do
   {result: UsersController.create(params)}.to_json
 end
 
-post '/orders' do
+post '/:user_id/orders' do
   {result: OrdersController.new(params[:user_id]).create(params)}.to_json
 end
